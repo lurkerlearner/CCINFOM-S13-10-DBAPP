53b182d4e76f554216bf48b1217c9778fa200b89
Sarah Nicole Jallorina <rahneejall@gmail.com>
2025-10-31 20:54:22 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = DBConnection.getConnection();
+        if (conn != null) {
+            System.out.println("Successfully connected to floodpanda database!");
+        } else {
+            System.out.println("Failed to connect to MySQL.");
+        }
+    }
+}
