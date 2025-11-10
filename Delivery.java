217d4f716a0474083fc29a7e2b00a0456a61bd2a
Sarah Nicole Jallorina <rahneejall@gmail.com>
2025-11-10 20:27:18 +0800

    Edited enum types in Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -4,34 +4,35 @@ import java.sql.Time;
 public class Delivery 
 {
     private int transaction_id;
-    private int client_id;
-    private int meal_id;
     private Date order_date;
     private Time time_ordered;
     private Time time_delivered;
-    private String payment_mode;
-    private String payment_status;
+    private PaymentMode payment_mode;
+    private PaymentStatus payment_status;
+    private DeliveryMethod delivery_method;
+    private DeliveryStatus delivery_status;
+    private int client_id;
+    private int meal_id;
     private int rider_id;
-    private String delivery_status;
-    private String delivery_method;
 
-    public Delivery(int transaction_id, int client_id, int meal_id,
-                    Date order_date, Time time_ordered, Time time_delivered,
-                    int rider_id, String payment_mode, String payment_status,
-                    String delivery_status, String delivery_method)
+    public Delivery(int transaction_id, Date order_date, Time time_ordered, Time time_delivered,
+                    PaymentMode payment_mode, PaymentStatus payment_status, DeliveryMethod delivery_method, 
+                    DeliveryStatus delivery_status, int client_id, int meal_id, int rider_id)
                     {
                         this.transaction_id = transaction_id;
-                        this.client_id = client_id;
-                        this.meal_id = meal_id;
                         this.order_date = order_date;
                         this.time_ordered = time_ordered;
                         this.time_delivered = time_delivered;
-                        this.rider_id = rider_id;
                         this.payment_mode = payment_mode;
                         this.payment_status = payment_status;
-                        this.delivery_status = delivery_status;
                         this.delivery_method = delivery_method;
+                        this.delivery_status = delivery_status;
+                        this.client_id = client_id;
+                        this.meal_id = meal_id;
+                        this.rider_id = rider_id;
                     }
+
+    public Delivery() {}
 
     public int getTransactionID() 
     {
@@ -68,22 +69,22 @@ public class Delivery
         return rider_id;
     }
 
-    public String getPaymentMode()
+    public PaymentMode getPaymentMode()
     {
         return payment_mode;
     }
 
-    public String getPaymentStatus()
+    public PaymentStatus getPaymentStatus()
     {
         return payment_status;
     }
 
-    public String getDeliveryStatus()
+    public DeliveryStatus getDeliveryStatus()
     {
         return delivery_status;
     }
 
-    public String getDeliveryMethod()
+    public DeliveryMethod getDeliveryMethod()
     {
         return delivery_method;
     }
@@ -123,22 +124,22 @@ public class Delivery
         this.rider_id = rider_id;
     }
 
-    public void setPaymentMode(String payment_mode)
+    public void setPaymentMode(PaymentMode payment_mode)
     {
         this.payment_mode = payment_mode;
     }
 
-    public void setPaymentStatus(String payment_status)
+    public void setPaymentStatus(PaymentStatus payment_status)
     {
         this.payment_status = payment_status;
     }
 
-    public void setDeliveryStatus(String delivery_status)
+    public void setDeliveryStatus(DeliveryStatus delivery_status)
     {
         this.delivery_status = delivery_status;
     }
 
-    public void setDeliveryMethod(String delivery_method)
+    public void setDeliveryMethod(DeliveryMethod delivery_method)
     {
         this.delivery_method = delivery_method;
     }
